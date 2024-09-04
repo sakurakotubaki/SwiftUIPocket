f2ac87fdb24c5d5a9fe74206ab53422443e3804e
Jboy422 <gitonly543@gmail.com>
2024-09-05 07:34:58 +0900

    Initial Commit

---
+++ SwiftUIPocket/SwiftUIPocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPocketApp.swift
+//  SwiftUIPocket
+//
+//  Created by Jboy422 on 2024/09/05.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
