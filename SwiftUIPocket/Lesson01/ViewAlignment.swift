6fdac9e4b07242992bff2dcf851e721894d8c834
Jboy422 <gitonly543@gmail.com>
2024-09-06 07:44:53 +0900

    added: ui

---
+++ SwiftUIPocket/Lesson01/ViewAlignment.swift
@@ -0,0 +1,48 @@
+import SwiftUI
+
+struct ViewAlignment: View {
+    var body: some View {
+        VStack {
+            // top guides
+            HStack {
+                Rectangle()
+                                             .frame(width: 20,height: 20)
+                    .frame(alignment: .topLeading)
+                Rectangle()
+                                             .frame(width: 20,height: 20)
+                    .frame(alignment: .top)
+                Rectangle()
+                                             .frame(width: 20,height: 20)
+                    .frame(alignment: .topTrailing)
+            }
+            // middle guides
+            HStack {
+                Rectangle()
+                                             .frame(width: 20,height: 20)
+                    .frame(alignment: .leading)
+                Rectangle()
+                                             .frame(width: 20,height: 20)
+                    .frame(alignment: .center)
+                Rectangle()
+                                             .frame(width: 20,height: 20)
+                    .frame(alignment: .trailing)
+            }
+            // bottom guides
+            HStack {
+                Rectangle()
+                                             .frame(width: 20,height: 20)
+                    .frame(alignment: .bottom)
+                Rectangle()
+                                             .frame(width: 20,height: 20)
+                    .frame(alignment: .bottom)
+                Rectangle()
+                                             .frame(width: 20,height: 20)
+                    .frame(alignment: .bottomTrailing)
+            }
+        }
+    }
+}
+
+#Preview {
+    ViewAlignment()
+}
