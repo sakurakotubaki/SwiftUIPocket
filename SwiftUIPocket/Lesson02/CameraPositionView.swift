27a0b3c01513e06721dd3e3856fee63eb7686eaa
Jboy422 <gitonly543@gmail.com>
2024-09-12 08:17:26 +0900

    layout: map

--- SwiftUIPocket/Lesson02/CameraPositionView.swift
+++ SwiftUIPocket/Lesson02/CameraPositionView.swift
@@ -1,8 +1,19 @@
 import SwiftUI
+import MapKit
 
 struct CameraPositionView: View {
+    @State var cameraPosition: MapCameraPosition = MapCameraPosition.camera(
+        MapCamera(centerCoordinate: CLLocationCoordinate2D(latitude: 35.6809591, longitude: 139.7673068), distance: 3729, heading: 92, pitch: 70))
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Map(position: $cameraPosition)
+            .mapControls {
+                MapUserLocationButton()
+                MapCompass()
+                MapScaleView()
+                MapPitchToggle()
+            }
+            .mapStyle(.standard)
     }
 }
 
